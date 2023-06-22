d7f0d63bb86907f34768e80ccd859be3679313af
Cihan <ozcancihan714@gmail.com>
2023-06-22 19:47:08 +0300

    updated auth.js

--- routers/auth.js
+++ routers/auth.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, logout } = require("../controllers/auth");
+const {
+  register,
+  login,
+  logout,
+  generateNewAccessToken,
+} = require("../controllers/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
+router.post("/rf_token", generateNewAccessToken);
 
 module.exports = router;
